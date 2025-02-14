5dc6316247749ed1eb490b6a0563a2ad1d964f18
Cory Huebner <chuebner86@gmail.com>
2025-02-14 07:36:06 -0600

    fixed crab dancing

--- src/datasets/results.js
+++ src/datasets/results.js
@@ -10,7 +10,7 @@ export const results = {
     'Group Dancing': { url: 'https://giphy.com/embed/DhstvI3zZ598Nb1rFf' },
     'Stylish Dancing': { url: 'https://giphy.com/embed/6fScAIQR0P0xW' },
     'Laid Back Dancing': { url: 'https://giphy.com/embed/VzH4jP7ppWF8dgCxcg' },
-    'Crab Dancing': { url: 'https://giphy.com/embed/VzH4jP7ppWF8dgCxcg' },
+    'Crab Dancing': { url: 'https://giphy.com/embed/aoegxxFogBQm74XCWY' },
     'Toothless': { url: 'https://giphy.com/embed/jtMqCDl7E07mM'},
     'Jurassic World': { url: 'https://giphy.com/embed/3rgXBvnbXtxwaWmhr2' },
     'Black Panther': { url: 'https://giphy.com/embed/efKmkwIKmMhMkzInnm' },
